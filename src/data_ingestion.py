24e0890684b1c8540cf8565d8dc7fd6536bab307
prashan-123 <prashanvardhan@gmail.com>
2025-05-14 12:26:14 +0530

    initial commit pc2

---
+++ src/data_ingestion.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import os
+
+df=pd.read_csv(r"P:\awss3\data\raw\student_placement_data.csv")
+X=df.drop(columns=['PLACED'])
+y=df['PLACED']
+
+ss=StandardScaler()
+X_scaled=ss.fit_transform(X)
+
+pca=PCA(n_components=2)
+X_pca=pca.fit_transform(X_scaled)
+
+df_pca=pd.DataFrame(data=X_pca,columns=["PC1","PC2"])
+df_pca["Placed"]=y.values
+
+df_pca.to_csv(os.path.join('data','processed','student_perfromance_pca.csv'),index=False)
