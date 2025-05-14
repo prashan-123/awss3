9e1e0604eceae0ab048cb7cd5b6253c488775c37
prashan-123 <prashanvardhan@gmail.com>
2025-05-14 12:39:36 +0530

    add pc3

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -10,10 +10,10 @@ y=df['PLACED']
 ss=StandardScaler()
 X_scaled=ss.fit_transform(X)
 
-pca=PCA(n_components=2)
+pca=PCA(n_components=3)
 X_pca=pca.fit_transform(X_scaled)
 
-df_pca=pd.DataFrame(data=X_pca,columns=["PC1","PC2"])
+df_pca=pd.DataFrame(data=X_pca,columns=["PC1","PC2","PC3"])
 df_pca["Placed"]=y.values
 
 df_pca.to_csv(os.path.join('data','processed','student_perfromance_pca.csv'),index=False)
